1a037bfe97b52084375d0093c92f8e2898b8b9cc
ROBERTA NASCIMENTO <ROBERTA.NASCIMENTO@hospitalsantamarta.com.br>
2026-04-30 15:15:00 -0300

    Desafio de cores cumprido

--- AULA 3/script.js
+++ AULA 3/script.js
@@ -1,28 +1,30 @@
 function adicionarTarefa() {
 
     //recebe valor do input do usuário
-    const inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value.trim()
+    const inputTarefa = document.getElementById("inputTarefa");
+    let tarefa = inputTarefa.value.trim();
 
-    const mensagem = document.getElementById("mensagem")
+    const mensagem = document.getElementById("mensagem");
 
     // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
     if (tarefa == "") {
         //mostre uma mensagem de erro
-        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
-        mensagem.textContent = mensagemErro
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!";
+        mensagem.textContent = mensagemErro;
+        mensagem.style.color = "#A34743";
     } else {
         //mensagem de tarefa adicionada com sucesso
-        let mensagemSucesso = "Tarefa adicionada com sucesso!"
-        mensagem.textContent = mensagemSucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        mensagem.textContent = mensagemSucesso;
+        mensagem.style.color = "#28A745";
 
         //cria novo item (li) e insere na (lista ul)
-        const listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
-        novaTarefa.textContent = tarefa
+        const listaTarefas = document.getElementById("listaTarefas");
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefa;
         listaTarefas.appendChild(novaTarefa)
     }
 
     //limpa o input do usuário
-    inputTarefa.value = ""
+    inputTarefa.value = "";
 }
